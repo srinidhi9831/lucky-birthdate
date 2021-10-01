2d6a710e30da21c8948706c5f8c1bab50e5c76f3
srinidhi9831 <srinidhi9831@gmail.com>
2021-10-01 11:49:26 +0530

    bug: fixed
    not getting required output

--- birth.js
+++ birth.js
@@ -1,18 +1,19 @@
 function sum(date){
-    var dateSum=0;
-    while(date>0){
-        dateSum=dateSum+ (date%10);
-        date= Math.floor(date/10);
-        //console.log(dateSum);
+    var newDate=0;
+    for(var i=0;i<date.length;i++)
+    {
+          if(!isNaN(date[i])){
+              newDate=newDate+parseInt(date[i]);
+          }
+
     }
-    console.log(dateSum);
-    return dateSum;
+    return newDate;
 }
 
 function isLucky(){
     
     var db=document.getElementById("date").value;
-    var luckyNumber=document.getElementById("lucky").value;
+    var luckyNumber=parseInt(document.getElementById("lucky").value);
     if(sum(db)%luckyNumber==0)
     {
         document.getElementById("output-div").innerText="you got a LUCKY birth date!!";
